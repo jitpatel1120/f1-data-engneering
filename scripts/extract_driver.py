7a03461939ef413a0f0f2bc59a56436c950c2380
Jit Patel <jitpatel@JBs-MBP.local>
2026-09-24 22:41:47 -0400

    added laps extraction logic

--- scripts/extract_driver.py
+++ scripts/extract_driver.py
@@ -8,15 +8,22 @@ REQUEST_DELAY = 0.5
 LIMIT = 100
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
 
-def extract_endpoint(endpoint: str, season: int = None):
+def extract_endpoint(endpoint: str, season: int = None, round_number: int = None):
+    if round_number is not None and season is None:
+        raise ValueError("round_number cannot be provided without season")
+
     offset = 0  # Set the offset to 0 for the first page of results
 
     if season is None:
         url_base = f"https://api.jolpi.ca/ergast/f1/{endpoint}/"
         file_prefix = endpoint
+    elif round_number is None:
+            url_base = f"https://api.jolpi.ca/ergast/f1/{season}/{endpoint}/"
+            file_prefix = f"{endpoint}_{season}"
     else:
-        url_base = f"https://api.jolpi.ca/ergast/f1/{season}/{endpoint}/"
-        file_prefix = f"{endpoint}_{season}"
+        url_base = f"https://api.jolpi.ca/ergast/f1/{season}/{round_number}/{endpoint}/"
+        file_prefix = f"{endpoint}_{season}_{round_number}"
+
     while True:
         try:
             url = f"{url_base}?limit={LIMIT}&offset={offset}"
@@ -60,11 +67,42 @@ def backfill_races(start_season: int, end_season: int):
         else:
             print(f"Data for races in season {season} already exists. Skipping extraction.")
 
+
+def backfill_laps(start_season: int, end_season: int):
+    for season in range(start_season, end_season + 1):
+
+        file_path = PROJECT_ROOT / "data" / "raw" / "races" / f"races_{season}_offset_0.json"
+
+        if not file_path.exists():
+            extract_endpoint("races", season=season)
+            
+       
+        with open(file_path, "r", encoding="utf-8") as file:
+            data = json.load(file)
+
+            races = data["MRData"]["RaceTable"]["Races"]
+
+        for race in races:
+            round_number = race["round"]
+            lap_file_path = PROJECT_ROOT / "data" / "raw" / "laps" / f"laps_{season}_{round_number}_offset_0.json"
+
+            if not lap_file_path.exists():
+                extract_endpoint("laps", season=season, round_number=round_number)
+                #print(f"extracting for round {round_number}")
+            else:
+                print(f"Lap data for season {season}, round {round_number} already exists. Skipping extraction.")
+                    
+                    
+                    
+
+
 if __name__ == "__main__":
     #extract_endpoint("drivers")
     #extract_endpoint("constructors")
     #extract_endpoint("circuits")
     #extract_endpoint("races", season=2026)
     #extract_endpoint("results", season=2025)
-    extract_endpoint("qualifying", season=2025)
+    #extract_endpoint("qualifying", season=2025)
     #backfill_races(2024, 2026)
+    #extract_endpoint("laps", season=2025, round_number=1)
+    #backfill_laps(2025, 2025)
